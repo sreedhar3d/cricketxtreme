1d3f7beeca1fe02ac9c8a68f537258a25298e218
kinshuksunil <kinshuksunil@gmail.com>
2010-06-14 18:25:33 +0000

    added aptana project files and created initial repository

---
+++ src/class/player.py
@@ -0,0 +1,16 @@
+'''
+Created on Jun 14, 2010
+@author: Kinshuk Sunil
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        
